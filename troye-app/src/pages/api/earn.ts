dd62b098be16d26295fd55c888a2db53fe207c0e
jjperezaguinaga <me@jjperezaguinaga.com>
2023-03-26 20:18:04 +0000

    feature: Ensure UI added transaction

--- troye-app/src/pages/api/earn.ts
+++ troye-app/src/pages/api/earn.ts
@@ -3,15 +3,21 @@ import { hex2buf } from '@/helpers/buffers';
 import { WebauthnChallenge } from '@/helpers/webauthn';
 import { submitAttestation } from '@/lib/atst';
 import { QRPayload } from '@/types/qrcode';
-import { providers, Wallet } from 'ethers';
+import { ethers, providers, Wallet } from 'ethers';
 import { NextApiRequest, NextApiResponse } from 'next';
 const { getHash } = require("emoji-hash-gen");
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export type EarnReponse = {
+  status: 'ok' | 'err',
+  message: string,
+  transactionHash?: string
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<EarnReponse>) {
   try {
     // Ensure request method is POST
     if (req.method !== 'POST') {
-      res.status(405).json({ message: 'Method not allowed' });
+      res.status(405).json({ status: 'err', message: 'Method not allowed' });
       return;
     }
 
@@ -35,15 +41,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const attestationValueAsString = `${troyePublicKeyAsHexHashed},${block.timestamp}`
         const publicKeyAsHex = getHash(signedPayloadByStore.publicKeyAsHex)
         console.log("Submitting attestation", publicKeyAsHex, attestationValueAsString)
-        const rcptx = await submitAttestation(wallet, provider, publicKeyAsHex, attestationValueAsString);
-        res.status(200).json({ message: 'attested' });
+        const tx = await submitAttestation(wallet, provider, publicKeyAsHex, attestationValueAsString);
+        res.status(200).json({ status: 'ok', message: 'Attestation created', transactionHash: tx.transactionHash });
     } else {
         console.error('Payload is too old:', signedPayloadByStore.blockchainNumber, block.number);
-        res.status(500).json({ message: 'Payload is too old' });
+        res.status(500).json({ status: 'err', message: 'Payload is too old' });
         return;
     }
   } catch (error) {
     console.error('Error processing payload:', error);
-    res.status(500).json({ message: 'Error processing payload' });
+    res.status(500).json({ status: 'err', message: 'Error processing payload' });
   }
 }
--- troye-app/src/pages/earn.tsx
+++ troye-app/src/pages/earn.tsx
@@ -1,72 +1,83 @@
-import { BarcodeScanner } from "@/components/BarcodeScanner";
-import { Tagline } from "@/components/Tagline";
-import { TroyeId } from "@/components/TroyeId";
-import { buf2hex } from "@/helpers/buffers";
-import { Verification, verifyPublicKeyAndSignature } from "@/helpers/verify";
-import { QRPayload } from "@/types/qrcode";
-import { Flex, Button, Text } from "@chakra-ui/react";
+import { BarcodeScanner } from '@/components/BarcodeScanner'
+import { Link as ChakraLink } from '@chakra-ui/react'
+import { Tagline } from '@/components/Tagline'
+import { TroyeId } from '@/components/TroyeId'
+import { buf2hex } from '@/helpers/buffers'
+import { Verification, verifyPublicKeyAndSignature } from '@/helpers/verify'
+import { QRPayload } from '@/types/qrcode'
+import { Flex, Button, Text } from '@chakra-ui/react'
 import Head from 'next/head'
-import { useEffect, useState } from "react";
-const { getHash } = require("emoji-hash-gen");
+import { useEffect, useState } from 'react'
+import { EarnReponse } from './api/earn'
+const { getHash } = require('emoji-hash-gen')
 
 const Earn = () => {
-  const [isLoading, setLoading] = useState(false);
-  const [data, setDataResponse] = useState<string | null>(null);
-  const [isCameraDisplayed, setIsCameraDisplayed] = useState(false);
-  const [qrcodeValue, setQRCodeValue] = useState<string | null>(null);
-  const [qrcodePayload, setQRCodePayload] = useState<QRPayload | null>(null);
-  const [publicKeyAsHexAndHashed, setPublicKeyAsHexAndHashed] = useState<string | null>(null);
-  const [troyePublicKey, setTroyePublicKey] = useState<ArrayBuffer | null>(null);
-  const [troyePublicKeyAsHexHashed, setTroyePublicKeyAsHexHashed] = useState<string | null>(null);
-  const [assertion, setAssertion] = useState<PublicKeyCredential | null>(null);
-  const [verification, setVerification] = useState<Verification | null>(null);
+  const [isLoading, setLoading] = useState(false)
+  const [response, setResponse] = useState<EarnReponse | null>(null)
+  const [isCameraDisplayed, setIsCameraDisplayed] = useState(false)
+  const [qrcodeValue, setQRCodeValue] = useState<string | null>(null)
+  const [qrcodePayload, setQRCodePayload] = useState<QRPayload | null>(null)
+  const [publicKeyAsHexAndHashed, setPublicKeyAsHexAndHashed] = useState<
+    string | null
+  >(null)
+  const [troyePublicKey, setTroyePublicKey] = useState<ArrayBuffer | null>(null)
+  const [troyePublicKeyAsHexHashed, setTroyePublicKeyAsHexHashed] = useState<
+    string | null
+  >(null)
+  const [assertion, setAssertion] = useState<PublicKeyCredential | null>(null)
+  const [verification, setVerification] = useState<Verification | null>(null)
 
   useEffect(() => {
     if (qrcodeValue) {
-      const payload = JSON.parse(qrcodeValue) as QRPayload;
-      console.log("Payload", payload);
-      setQRCodePayload(payload);
-      setIsCameraDisplayed(false);
+      const payload = JSON.parse(qrcodeValue) as QRPayload
+      console.log('Payload', payload)
+      setQRCodePayload(payload)
+      setIsCameraDisplayed(false)
     }
-  }, [qrcodeValue]);
+  }, [qrcodeValue])
 
   useEffect(() => {
     const loadVerification = async () => {
-      const verification = await verifyPublicKeyAndSignature(troyePublicKey, assertion);
-      console.log("Verification", verification);
+      const verification = await verifyPublicKeyAndSignature(
+        troyePublicKey,
+        assertion
+      )
+      console.log('Verification', verification)
       if (verification && verification.isValid) {
         setVerification(verification)
       }
     }
     if (troyePublicKey && assertion) {
-      loadVerification();
+      loadVerification()
     }
-    return (() => {
-      setVerification(null);
-    })
-  }, [assertion]);
+    return () => {
+      setVerification(null)
+    }
+  }, [assertion])
 
   useEffect(() => {
     if (qrcodePayload) {
-      setPublicKeyAsHexAndHashed(getHash(qrcodePayload.publicKeyAsHex || 'empty'));
+      setPublicKeyAsHexAndHashed(
+        getHash(qrcodePayload.publicKeyAsHex || 'empty')
+      )
     }
-    return (() => {
-      setPublicKeyAsHexAndHashed(null);
-    })
-  }, [qrcodePayload]);
+    return () => {
+      setPublicKeyAsHexAndHashed(null)
+    }
+  }, [qrcodePayload])
 
   useEffect(() => {
     if (troyePublicKey) {
-      setTroyePublicKeyAsHexHashed(getHash(buf2hex(troyePublicKey)));
+      setTroyePublicKeyAsHexHashed(getHash(buf2hex(troyePublicKey)))
     }
-    return (() => {
-      setTroyePublicKeyAsHexHashed(null);
-    })
-  }, [troyePublicKey]);
+    return () => {
+      setTroyePublicKeyAsHexHashed(null)
+    }
+  }, [troyePublicKey])
 
   async function sendVerification() {
     try {
-      setLoading(true);
+      setLoading(true)
       // Send a POST request to the API endpoint
       const response = await fetch('/api/earn', {
         method: 'POST',
@@ -74,18 +85,22 @@ const Earn = () => {
           'Content-Type': 'application/json',
         },
         // ArrayBuffers are not serializable, so we need to convert them to strings
-        body: JSON.stringify({ ...verification, clientDataJSON: buf2hex(verification.clientDataJSON), troyePublicKeyAsHexHashed }),
-      });
-  
+        body: JSON.stringify({
+          ...verification,
+          clientDataJSON: buf2hex(verification.clientDataJSON),
+          troyePublicKeyAsHexHashed,
+        }),
+      })
+
       // Parse the response as JSON
-      const data = await response.json();
-      setDataResponse(JSON.stringify(data));
+      const data = await response.json()
+      setResponse(data)
       // Log the response message
-      console.log('Response:', data.message);
-      setLoading(false);
+      console.log('Response:', data.message)
+      setLoading(false)
     } catch (error) {
-      console.error('Error sending payload:', error);
-      setLoading(false);
+      console.error('Error sending payload:', error)
+      setLoading(false)
     }
   }
 
@@ -93,7 +108,10 @@ const Earn = () => {
     <>
       <Head>
         <title>Troye - Earn</title>
-        <meta name="description" content="Troye is a platform to create loyalty programs" />
+        <meta
+          name="description"
+          content="Troye is a platform to create loyalty programs"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -111,40 +129,74 @@ const Earn = () => {
           mt={8}
           alignItems="center"
         >
-          <Button onClick={() => setIsCameraDisplayed(!isCameraDisplayed)}>{!isCameraDisplayed ? 'Display' : 'Hide'} Camera</Button>
-          {isCameraDisplayed && <BarcodeScanner setBarcodeValue={setQRCodeValue} />}
-          {qrcodePayload &&
-            <Flex direction="column" height="50vh" justifyContent="space-around">
+          <Button onClick={() => setIsCameraDisplayed(!isCameraDisplayed)}>
+            {!isCameraDisplayed ? 'Display' : 'Hide'} Camera
+          </Button>
+          {isCameraDisplayed && (
+            <BarcodeScanner setBarcodeValue={setQRCodeValue} />
+          )}
+          {qrcodePayload && (
+            <Flex
+              direction="column"
+              height="50vh"
+              justifyContent="space-around"
+            >
               <Flex direction="column">
                 <Text fontSize="4xl">{publicKeyAsHexAndHashed}</Text>
-                <Text color="gray.400" fontSize="sm">Store Public Identifier</Text>
+                <Text color="gray.400" fontSize="sm">
+                  Store Public Identifier
+                </Text>
               </Flex>
-              {troyePublicKeyAsHexHashed &&
+              {troyePublicKeyAsHexHashed && (
                 <Flex direction="column">
                   <Text fontSize="4xl">{troyePublicKeyAsHexHashed}</Text>
-                  <Text color="gray.400" fontSize="sm">Your Troye Id Public Identifier</Text>
+                  <Text color="gray.400" fontSize="sm">
+                    Your Troye Id Public Identifier
+                  </Text>
                 </Flex>
-              }
+              )}
               <TroyeId
                 publicKeyAsHex={qrcodePayload.publicKeyAsHex}
                 username={publicKeyAsHexAndHashed}
-                qrPayloadAsUint8Array={new TextEncoder().encode(JSON.stringify(qrcodePayload))}
+                qrPayloadAsUint8Array={new TextEncoder().encode(
+                  JSON.stringify(qrcodePayload)
+                )}
                 setTroyePublicKey={setTroyePublicKey}
                 setAssertion={setAssertion}
               />
             </Flex>
-          }
-          {
-            verification?.isValid &&
+          )}
+          {verification?.isValid && (
             <>
-            <Button isLoading={isLoading} onClick={sendVerification} colorScheme="green">Send Signed Troye Proof</Button>
-            <Text>{data && 'Signed submission sent'}</Text>
+              <Button
+                isLoading={isLoading}
+                onClick={sendVerification}
+                colorScheme="green"
+              >
+                Send Signed Troye Proof
+              </Button>
+
+              {response &&
+                (response?.status == 'ok' ? (
+                  <Text>
+                    Signed submission sent, see{' '}
+                    <ChakraLink
+                    style={{ textDecoration: 'underline' }}
+                      isExternal={true}
+                      href={`https://goerli-optimism.etherscan.io/tx/${response?.transactionHash}`}
+                    >
+                      transaction.
+                    </ChakraLink>
+                  </Text>
+                ) : (
+                  <Text>`Error sending submission: ${response?.message}` </Text>
+                ))}
             </>
-          }
+          )}
         </Flex>
       </main>
     </>
-  );
+  )
 }
 
-export default Earn;
+export default Earn
